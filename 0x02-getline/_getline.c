8e170e3af1558863347376f7c2f19c88a1cd69da
darbumas <2314@holbertonschool.com>
2022-05-19 16:01:08 -0500

    getline with multi-fd

--- 0x02-getline/_getline.c
+++ 0x02-getline/_getline.c
@@ -1,7 +1,7 @@
 #include "_getline.h"
 
-char *readbuf(char *, int);
-int _strlen(char *);
+FILE_T *append_file(int);
+char *flash_buf(FILE_T *, FILE_T **, FILE_T **);
 /**
  * _getline - reads an entire line from a file descriptor
  * @fd: file descriptor to read from
@@ -10,77 +10,88 @@ int _strlen(char *);
 
 char *_getline(const int fd)
 {
-	static char *buf;
-	char *line;
-	static int file, size;
+	static FILE_T *file;
+	FILE_T *doc = file;
+	char *string;
+	int i, j;
 
-	if (fd != file || size >= READ_SIZE)
+	if (!file)
 	{
-		file = fd;
-		buf = malloc(sizeof(char *) * READ_SIZE);
-		read(fd, buf, READ_SIZE);
-		size = 0;
-		line = strdup(buf);
-		line = readbuf(line, 1);
-		if (line != NULL)
-			size += _strlen(line) + 1;
-		if (buf[0] == '\000')
-			return (NULL);
-		return (line);
+		file = append_file(fd);
+		doc = file;
 	}
-	line = strdup(buf);
-	line = readbuf(line, 0);
-	if (line != NULL)
-		size += _strlen(line) + 1;
-	return (line);
-}
-
-/**
- * readbuf - returns text read from buffer
- * @buf: pointer to buffer
- * @file: indicates if file is present
- * Return: text from buffer
- */
-
-char *readbuf(char *buf, int file)
-{
-	char *txt;
-	static int alpha, omega;
-	int i = 0;
-
-	if (file)
-		omega = 0;
-	alpha = omega;
-	if (buf[omega] == '\000')
+	else
 	{
-		free(buf);
-		return (NULL);
+		while (doc)
+		{
+			if (doc->fd == fd)
+				break;
+			if (!doc->next)
+				doc->next = append_file(fd);
+			doc = doc->next;
+		}
 	}
-	txt = malloc(sizeof(char *) * READ_SIZE);
-	while (buf[omega] != '\n' && buf[omega] != '\000')
+	i = doc->one;
+	j = doc->two;
+	if (doc->buffer[i] == '\0' || fd == -1)
+		return (flash_buf(file, &file, &doc));
+	while (doc->buffer[i])
 	{
-		omega++;
+		if (doc->buffer[i] == '\n' || doc->buffer[i + 1] == '\0')
+		{
+			string = malloc(sizeof(char *) * (i - j));
+			strncpy(string, doc->buffer + j, i - j + 1);
+			j = i + 1;
+			doc->one = doc->two = j;
+			return (string);
+		}
 		i++;
 	}
-	strncpy(txt, buf + alpha, i);
-	omega++;
-	alpha++;
-	return (txt);
+	return (NULL);
 }
 
 /**
- * _strlen - returns string length
- * @str: pointer to string
- * Return: length
+ * append_file - returns a pointer to a struct with a read file in buffer.
+ * @fd: file descriptor
+ * Return: pointer to a struct
  */
-int _strlen(char *str)
+FILE_T *append_file(int fd)
 {
-	int len = 0;
+	FILE_T *pointer;
 
-	while (*str)
+	pointer = malloc(sizeof(FILE_T));
+	pointer->fd = fd;
+	pointer->buffer = malloc(sizeof(char *) * READ_SIZE);
+	read(fd, pointer->buffer, READ_SIZE);
+	pointer->one = 0;
+	pointer->two = 0;
+	return (pointer);
+}
+
+/**
+ * flash_buf - frees up memory
+ * @head: head node
+ * @file: pointer to pointer to files
+ * @doc: pointer to pointer to files
+ * Return: NULL
+ */
+char *flash_buf(FILE_T *head, FILE_T **file, FILE_T **doc)
+{
+	FILE_T *next;
+
+	while (head)
 	{
-		str++;
-		len++;
+		next = (*head).next;
+		free((*head).buffer);
+		free(head);
+		if (next == NULL)
+			head = NULL;
+		else
+			head = next;
 	}
-	return (len);
+	free(head);
+	free(next);
+	*file = NULL;
+	*doc = NULL;
+	return (NULL);
 }
--- 0x02-getline/_getline.h
+++ 0x02-getline/_getline.h
@@ -8,6 +8,21 @@
 
 #define READ_SIZE 1024
 
+/**
+ * struct fildes - defines a struct for a file descriptor
+ * @fd: file descriptor
+ * @one: tracks characters
+ * @two: tracks characters
+ * @buffer: buffer to read from
+ * @next: points to the next node
+ */
+typedef struct fildes
+{
+	int fd, one, two;
+	char *buffer;
+	struct fildes *next;
+} FILE_T;
+
 char *_getline(const int);
 
 #endif
