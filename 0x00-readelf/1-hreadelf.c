be97ada06c256a54d5d9d6fbb39fd2042ede4510
darbumas <2314@holbertonschool.com>
2022-06-22 13:28:42 -0500

    readelf -W -S

---
+++ 0x00-readelf/1-hreadelf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - displays info contained in ELF file header
+ * @argc: argument count
+ * @argv: arguments
+ * @env: environment
+ * Return: 0 on success
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *args[] = {"/usr/bin/readelf", "-W", "-S", "", NULL};
+
+	(void) argc;
+	args[3] = argv[1];
+	if (execve("/usr/bin/readelf", args, env) == -1)
+	{
+		perror("execve");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
