8e941685b80227278d6d471368b48f87576bf42d
darbumas <2314@holbertonschool.com>
2022-07-04 20:04:43 -0500

    third attempt

--- 0x01-proc_filesystem/read_write_heap.py
+++ 0x01-proc_filesystem/read_write_heap.py
@@ -20,39 +20,61 @@ def main():
 
     if len(replaceStr) > len(searchStr):
         exit(1)
-    
-    mapsFile = "/proc/{}/maps".format(pid)
-    memFile = "/proc/{}/mem".format(pid)
+    try:
+        mapsFile = open("/proc/{}/maps".format(pid), 'r')
+    except OSError as e:
+        print("Unable to open file proc/{}/maps\nOSError : {}".format(pid, e))
+        exit(1)
 
-    heap = {
-        "start": '',
-        "end": '',
-        "mode": '',
-        "mem": ''
-    }
-    with open(mapsFile) as maps:
-        for line in maps:
-            if "[heap]" in line:
-                s = line.split()
-                s[0] = s[0].split('-')
-                s[0] = list(map(lambda x: int('0x' + x, 16), s[0]))
-                [heap['start'], heap['end']] = s[0]
-                heap['mode'] = s[1]
-                break
-    if heap['start'] == '':
-        print("Missing heap")
-        exit(2)
+    print("[*] maps: /proc/{}/maps".format(pid))
+    print("[*] mem: /proc/{}/mem".format(pid))
 
-    with open(memFile, 'rb+') as mem:
-        mem.seek(heap['start'])
-        heap['mem'] = mem.read(heap['end'] - heap['start'])
-        try:
-            i = heap['mem'].index(bytes(searchStr, "ASCII"))
-        except ValueError:
-            print("Not matching string")
-            exit(3)
-        mem.seek(heap['start'] + i)
-        mem.write(bytes(replaceStr, "ASCII"))
+    heapFound = None
+    for line in mapsFile:
+        if "heap" in line:
+            heapFound = line.split()
+    mapsFile.close()
+
+    if heapFound is None:
+        print("No heap found!")
+        exit(1)
+    else:
+        print('\n'.join(("[*] Found: {}".format(heapFound[-1]),
+                         "\tpathname = {}".format(heapFound[-1]),
+                         "\taddr range = {}".format(heapFound[0]),
+                         "\tperm = {}".format(heapFound[1]),
+                         "\toffset (in bytes) = {}".format(heapFound[2]),
+                         "\tinode = {}".format(heapFound[4]))))
+    addr = heapFound[0].split('-')
+    print("[*] Addresses start [{}] | end [{}]".format(addr[0].lstrip('0'),
+                                                       addr[1].lstrip('0')))
+
+    perms = heapFound[1]
+    if 'r' not in perms:
+        print("Heap does not have read permission")
+        exit(0)
+    if 'w' not in perms:
+        print("Heap does not have write permission")
+        exit(0)
+
+    try:
+        memFile = open("/proc/{}/mem".format(pid), 'rb+')
+    except OSError as e:
+        print("Can't open file /proc/{}/mem: OSError: {}".format(pid, e))
+        exit(1)
+
+    heapStart = int(addr[0], 16)
+    heapEnd = int(addr[1], 16)
+    memFile.seek(heapStart)
+    heap = memFile.read(heapEnd - heapStart)
+    offset = heap.find(bytes(searchStr, "ASCII"))
+    if offset == -1:
+        print("can't find {} in /proc/{}/mem".format(searchStr, pid))
+        exit(1)
+    else:
+        print("[*] Writing '{}' at {}".format(replaceStr,
+                                              hex(heapStart + offset)[2:]))
+
 
 if __name__ == "__main__":
     main()
