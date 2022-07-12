1b2ec349871d04f0c9f72e54f6f3307db2f06fe2
darbumas <2314@holbertonschool.com>
2022-07-12 11:25:30 -0500

    new dir + files

---
+++ 0x00-signals/signals.h
@@ -0,0 +1,14 @@
+#ifndef SIGNALS_H
+#define SIGNALS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int handle_signal(void);
+void (*current_handler_signal(void))(int);
+int handle_sigaction(void);
+void (*current_handler_sigaction(void))(int);
+int trace_signal_sender(void);
+
+#endif
