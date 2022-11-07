2d103f323411a0a14ea7762983f298a4b4b17b74
darbumas <2314@holbertonschool.com>
2022-11-07 13:49:20 -0600

    update header with prototypes

---
+++ 0x00-signals/signals.h
@@ -0,0 +1,15 @@
+#ifndef SIGNALS_H
+#define SIGNNALS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int handle_signal(void);
+void (*current_handler_signal(void))(int);
+int handle_sigaction(void);
+void (*current_handler_sigaction(void))(int);
+int trace_signal_sender(void);
+int pid_exist(pid_t pid);
+
+#endif
