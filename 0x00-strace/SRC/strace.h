33e897b695b11867f747a13bc2fa464859a3ab8d
darbumas <2314@holbertonschool.com>
2023-01-12 02:57:00 +0000

    first trial at prob #1

---
+++ 0x00-strace/SRC/strace.h
@@ -0,0 +1,17 @@
+#ifndef _STRACE_H_
+#define _STRACE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <x86_64-linux-gnu/sys/user.h>
+
+
+#endif /* _STRACE_H_ */
