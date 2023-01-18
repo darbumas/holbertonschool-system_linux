1a931325a7347bcbd5281ce226a74f7179f58021
darbumas <2314@holbertonschool.com>
2023-01-18 06:16:59 +0000

    adding prototypes & more data structures

--- 0x01-multithreading/multithreading.h
+++ 0x01-multithreading/multithreading.h
@@ -1,5 +1,12 @@
+#ifndef _MULTITHREADING_H
+#define _MULTITHREADING_H
+
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "list.h"
 
 /**
  * struct pixel_s - RGB pixel
@@ -62,3 +69,51 @@ typedef struct blur_portion_s
     size_t h;
     kernel_t const *kernel;
 } blur_portion_t;
+
+typedef void *(*task_entry_t)(void *);
+
+/**
+ * enum task_status_e - Task statuses
+ *
+ * @PENDING: Task is pending
+ * @STARTED: Task has been started
+ * @SUCCESS: Task has completed successfully
+ * @FAILURE: Task has completed with issues
+ */
+typedef enum task_status_e
+{
+    PENDING = 0,
+    STARTED,
+    SUCCESS,
+    FAILURE
+} task_status_t;
+
+/**
+ * struct task_s - Executable task structure
+ *
+ * @entry:  Pointer to a function to serve as the task entry
+ * @param:  Address to a custom content to be passed to the entry function
+ * @status: Task status, default to PENDING
+ * @result: Stores the return value of the entry function
+ * @lock:   Task mutex
+ */
+typedef struct task_s
+{
+    task_entry_t    entry;
+    void        *param;
+    task_status_t   status;
+    void        *result;
+    pthread_mutex_t lock;
+} task_t;
+
+void *thread_entry(void *arg);
+int tprintf(char const *format, ...);
+void blur_portion(blur_portion_t const *portion);
+void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel);
+int tprintf(char const *format, ...);
+list_t *prime_factors(char const *s);
+task_t *create_task(task_entry_t entry, void *param);
+void destroy_task(task_t *task);
+void *exec_tasks(list_t const *tasks);
+
+#endif /* MULTITHREADING_H */
