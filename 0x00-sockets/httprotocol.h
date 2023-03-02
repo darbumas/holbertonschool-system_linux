7bd0c987eccf6c7a6363bea25ff95774a60d1599
darbumas <2314@holbertonschool.com>
2023-03-02 06:20:46 +0000

    body parameters

--- 0x00-sockets/httprotocol.h
+++ 0x00-sockets/httprotocol.h
@@ -11,11 +11,27 @@
 
 #define CRLF "\r\n"
 #define PORT 8080
-#define BUFFER_SZ 1024
+#define BUFFER_SZ 8192
 
 int server_start(void);
 int roger_that(int);
 int parse_request(int, char *);
 int send_response(int, char *);
+
+/**
+ * struct post_s - Information needed for a HTTP POST request
+ *
+ * @id: id of current POST
+ * @title: title of current POST
+ * @desc: description of current POST
+ * @next: next POST in the linked list.
+ */
+typedef struct post_s
+{
+	int id;
+	char *title;
+	char *desc;
+	struct post_s *next;
+} post_t;
 
 #endif
